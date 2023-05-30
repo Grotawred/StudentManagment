5a2e343712d7272edcd80435d4d9e66fc5fbd1a5
Grotenred <grishavlacko@gmai.com>
2023-05-30 15:20:11 +0300

    Add Post realisation

--- src/main/java/com/example/StudentManagment/serviceImpl/StudentServiceImpl.java
+++ src/main/java/com/example/StudentManagment/serviceImpl/StudentServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class StudentServiceImpl implements StudentService {
+
     private StudentRepository studentRepository;
     @Override
     public List<Student> getAllStudents() {
